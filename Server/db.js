90d2b8c4b2c3c97ca0e174cfc6a3bd981d61b81c
AgusCortez <zomboyski@gmail.com>
2024-11-14 17:43:16 -0300

    Conexión a la base de datos

--- Server/db.js
+++ Server/db.js
@@ -1,25 +1,13 @@
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres.dqxbefojgyovvfohjnlz',
-  host: 'aws-0-us-east-1.pooler.supabase.com',            
-  database: 'postgres',  
-  password: 'd7qOyspVm00W0pyr',    
-  port: 6543,                   
+  host: 'aws-0-us-east-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'd7qOyspVm00W0pyr',
+  port: 6543,
 });
 
-// Este codigo es para probar la base!
-const obtenerParticipantes = async () => {
-  try {
-      const client = await pool.connect(); // Conectar al pool
-      const res = await client.query('SELECT * FROM Participantes'); // Consulta a la tabla
-      console.log('Participantes:', res.rows); // Mostrar los resultados en consola
-      client.release(); // Liberar el cliente
-  } catch (err) {
-      console.error('Error al obtener los participantes:', err.stack);
-  }
-};
+// Exporta el pool para que pueda ser utilizado en otros archivos
+module.exports = { pool };
 
-// Llamar a la función para obtener los participantes
-obtenerParticipantes();
